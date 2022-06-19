5c2ef3e646bc0b7794de06da5e9c2c1b68bff09f
ksg2388 <45422827+ksg2388@users.noreply.github.com>
2022-06-19 16:19:40 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import subprocess
 import sys
 import requests
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
+app.secret_key = "My_Key"
 es_host = "http://localhost:9200"
 
 @app.route('/')
@@ -18,6 +19,8 @@ def hello_world():
 
 @app.route('/SingUp', methods=['GET', 'POST'])
 def register():
+    es = Elasticsearch(es_host)
+
     if request.method == 'GET':
         return render_template("SingUp.html")
     else:
@@ -25,27 +28,48 @@ def register():
         name = request.form.get('name')
         password = request.form.get('password')
         passwordConfirm = request.form.get('passwordConfirm')
-
-        if not (id and name and password and passwordConfirm):
-            return "입력되지 않은 정보가 있습니다"
-        elif password != passwordConfirm:
-            return "비밀번호가 일치하지 않습니다"
-        else:
+        print(name)
+        print(password)
+        
+        if (id and name and password):
             doc = {
                     'id' : id,
                     'name' : name,
                     'password' : password
             }
-            res = es.index(index='user', id=i, document=doc)
+            res = es.index(index='user', id = id, document=doc)
             print(doc)
             print(res)
 
             return "회원가입 성공"
+        
+        else:
+            if id == "":
+                flash("id를 입력해주세요.")
+            elif name == "":
+                flash("닉네임을 입력해주세요.")
+            elif password == "":
+                flash("password를 입력해주세요.")
+            elif password != passwordConfirm:
+                flash("password를 확인해주세요.")
+            return render_template("SingUp.html")
+
+        return redirect('/')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    es = Elasticsearch(es_host)
+
+    if request.method == 'GET':
+        return render_template("Login.html")
+    else:
+        id = request.form.get('id')
+        password = request.form.get('password')
+        print(id)
+        print(password)
         return redirect('/')
 
 
 if __name__ == '__main__':
-
-    es = Elasticsearch(es_host)
 
     app.run(host="127.0.0.1", port=5000, debug=True)
